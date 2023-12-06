6093608fe756e8ae89f6cafb4d2d04b21a1db2ca
Krym <gumimash@gmail.com>
2023-12-06 08:48:39 +0200

    InsertionSort in cpp + fixed names

---
+++ InsertionSort-Cpp/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+for (int i = 1; i < n; i++) {
+int temp = arr[i];
+int j = i - 1;
+
+while (j >= 0 && arr[j] > temp) {
+arr[j + 1] = arr[j];
+j--;
+}
+
+arr[j + 1] = temp;
+}
+}
+
+int main() {
+int arr[] = {12, 11, 13, 5, 6};
+int n = sizeof(arr) / sizeof(arr[0]);
+cout << "Unsorted array: ";
+for (int i = 0; i < n; i++) {
+cout << arr[i] << " ";
+}
+insertionSort(arr, n);
+
+cout << "Sorted array: ";
+for (int i = 0; i < n; i++) {
+cout << arr[i] << " ";
+}
+
+return 0;
+}
